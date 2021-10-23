672dbaf6760857f3f69924ddaa2945bb3d668e6f
Luiza Bozko Chagas <91279365+luizabchagas@users.noreply.github.com>
2021-10-23 13:39:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ function changeSocialMediaLinks() {
   }
 }
 
+/*function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[(0,1,2)].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}*/
+
 changeSocialMediaLinks()
 
 function getGitHubProfileInfos() {
@@ -29,16 +37,3 @@ function getGitHubProfileInfos() {
 }
 
 getGitHubProfileInfos()
-
-/* ARROW FUNCTIONS
-É uma forma contraída da função. Criamos uma função ANÔNIMA.
-Com isso, elimina-se a necessidade de dar um nome para a função e usar a palavra reservada function.
-E se tiver apenas um comando na função, não precisa colocar {}, como foi no caso acima.
-Então a declaração da função fica assim:
-argumento => {
-
-}
-ao invés de
-function nomeDaFuncao(argumentos) {
-
-}*/
